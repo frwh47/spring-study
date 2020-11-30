763fb68fcf4103675171df664ea1753301c7fc6a
sailor <sailor1@yeah.net>
2020-11-30 15:58:15 +0800

    rocketmq

---
+++ rocketmq/src/main/java/my/AsyncProducer.java
@@ -0,0 +1,57 @@
+package my;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.CountDownLatch2;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+
+public class AsyncProducer {
+    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr(Config.NAME_SERVER_ADDRESS);
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+        // 启动Producer实例
+        producer.start();
+
+        int messageCount = 100;
+        // 根据消息数量实例化倒计时计算器
+        final CountDownLatch2 countDownLatch = new CountDownLatch2(messageCount);
+        for (int i = 0; i < messageCount; i++) {
+            final int index = i;
+            // 创建消息，并指定Topic，Tag和消息体
+            Message msg = new Message("TopicTest",
+                    "TagA",
+                    "OrderID188",
+                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            // SendCallback接收异步返回结果的回调
+            producer.send(msg, new SendCallback() {
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    System.out.printf("%-10d OK %s %n", index,
+                            sendResult.getMsgId());
+                    countDownLatch.countDown();
+                }
+
+                @Override
+                public void onException(Throwable e) {
+                    System.out.printf("%-10d Exception %s %n", index, e);
+                    e.printStackTrace();
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        // 等待5s
+//        countDownLatch.await(5, TimeUnit.SECONDS);
+        // 如果不再发送消息，关闭Producer实例。
+        producer.shutdown();
+    }
+}
---
+++ rocketmq/src/main/java/my/Config.java
@@ -0,0 +1,5 @@
+package my;
+
+public class Config {
+    public static final String NAME_SERVER_ADDRESS = "10.200.5.13:9876";
+}
